714e092edc226fb1bb8a3e90d2ae01383072d690
ThanhThat <thachthanhthat1995@gmail.com>
2023-04-23 18:11:33 +0700

    Responsive for tablet and mobile.

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+(() => {
+  const nav = document.querySelector(".header-nav-mobile");
+  const btnClose = nav.querySelector(".btn-close-menu");
+  const menuItem = nav.querySelectorAll(".list-item > a");
+  const menuIcon = document.querySelector(".header-menu-mobile");
+  const overlay = document.querySelector(".overlay");
+
+  if (!nav || !btnClose || !menuItem || !menuIcon || !overlay) return false;
+
+  menuIcon.addEventListener("click", () => {
+    nav.classList.toggle("open-menu");
+    overlay.classList.toggle("active");
+  });
+
+  btnClose.addEventListener("click", () => {
+    overlay.classList.toggle("active");
+    nav.classList.toggle("open-menu");
+  });
+
+  overlay.addEventListener("click", () => {
+    nav.classList.toggle("open-menu");
+    overlay.classList.toggle("active");
+  });
+})();
